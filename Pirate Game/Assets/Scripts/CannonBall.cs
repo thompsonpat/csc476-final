ef32a24c64bb3ad95af3da7d7ffc7acc0245b0cf
thompsonpat <thompsonpat21@gmail.com>
2018-12-12 18:00:20 -0500

    cannon ball momentum from ship and wood spawning rotation

--- Pirate Game/Assets/Scripts/CannonBall.cs
+++ Pirate Game/Assets/Scripts/CannonBall.cs
@@ -12,22 +12,26 @@ public class CannonBall : MonoBehaviour
 	public GameObject parent;
 
 	private Rigidbody2D rb;
+    private Rigidbody2D parentRB;
 
     // Use this for initialization
     void Start()
     {
 		rb = GetComponent<Rigidbody2D>();
+        rb.velocity = parentRB.velocity;
 		Destroy(this.gameObject, range);
     }
 
     void FixedUpdate()
     {
+        // Shoot Cannon Ball Right
         rb.AddForce(transform.right * speed);
     }
 
 	void ParentInfo (GameObject parentGO)
 	{
 		parent = parentGO;
+        parentRB = parent.GetComponent<Rigidbody2D>();
 		Physics2D.IgnoreCollision(parent.GetComponent<Collider2D>(), GetComponent<Collider2D>());
 	}
 }
